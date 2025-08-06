895628b71fe44465b6c6306de3b1657082e390be
flaviomelian <flaviomelian2000@gmail.com>
2025-08-07 00:19:28 +0100

    cosas en linux añadidas

--- frontend/src/Pages/Projects/Projects.jsx
+++ frontend/src/Pages/Projects/Projects.jsx
@@ -45,6 +45,12 @@ import dashboardOdooPet from '../../assets/dashboard-odoo-pet.png'
 import formOdoo from '../../assets/form-odoo.png'
 import tailwind from '../../assets/tailwind.png'
 import bootstrap from '../../assets/bootstrap.png'
+import bash from '../../assets/bash.png'
+import ubuntu from '../../assets/ubuntu.png'
+import bashScript1 from '../../assets/bash-script-1-1.png'
+import bashScript2 from '../../assets/bash-script-1-2.png'
+import bashScript3 from '../../assets/bash-script-1-3.png'
+import effectBashScript from '../../assets/efecto-bash-script.png'
 import './Projects.css'
 
 const Projects = () => {
@@ -140,6 +146,22 @@ const Projects = () => {
                         <img className='cooks' src={cookbookFeed} />
                         <img className='cooks' src={cookbookNew} />
                     </div>
+                </div>
+                <div className='project'>
+                    <h2>Autoejecutable para backups</h2>
+                    <p>Un script para Linux en bash que permite crear copias de seguridad, paso a paso con un ejemplo.</p>
+                    <a href='' />
+                    <div className='stack-alt'>
+                        <img src={bash} />
+                        <img src={ubuntu}/>
+                    </div>
+                    <div className='bash-script'>
+                        <img src={bashScript1} />
+                        <img src={bashScript2} />
+                    </div>
+                    <img className='bash-script-img' src={bashScript3} />
+                    <p>El efecto es habernos creado una carpeta que anida el proyecto que queríamos respaldar, extensible a cualquier proyecto</p>
+                    <img className='bash-script-img' src={effectBashScript} />
                 </div>
                 <div className='project'>
                     <h2>Wallpaper Pattern Editor</h2>
