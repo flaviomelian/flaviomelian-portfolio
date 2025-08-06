0131f8fba75f876c83ddc5d1087188728c4951c6
flaviomelian <flaviomelian2000@gmail.com>
2025-08-06 19:36:24 +0100

    commandApp added to portfolio

--- frontend/src/Pages/Projects/Projects.jsx
+++ frontend/src/Pages/Projects/Projects.jsx
@@ -20,9 +20,25 @@ import cookbookNew from '../../assets/cookbook-new.jpeg'
 import wallpaper from '../../assets/wallpaper-generator.png'
 import spring from '../../assets/springboot-inner.svg'
 import react from '../../assets/react.svg'
+import next from '../../assets/next.png'
+import node from '../../assets/node.png'
+import commandAppDashBoard from '../../assets/commandApp-dashboard.png'
+import dailyMenu from '../../assets/daily-menu.png'
+import getCommand from '../../assets/get-command.png'
+import Commands from '../../assets/commands.png'
+import CommandDetail from '../../assets/command-detail.png'
+import TableStatus from '../../assets/table-status.png'
+import menus from '../../assets/menus.png'
+import dishes from '../../assets/dishes.png'
+import homeMobile from '../../assets/home-mobile.jpg'
+import dishesMobile from '../../assets/dishes.jpg'
+import tablesMobile from '../../assets/tables-mobile.jpg'
+import menusMobile from '../../assets/menus-mobile-details.jpg'
+import burgerMenu from '../../assets/burger-menu.jpg'
+import commandsMobile from '../../assets/comandas-mobile.jpg'
 import mysql from '../../assets/mysql.jpg'
 import python from '../../assets/python.png'
-import extApi from '../../assets/external-api.png' 
+import extApi from '../../assets/external-api.png'
 import odoo from '../../assets/odoo.png'
 import odooPet from '../../assets/odoo-pet.png'
 import dashboardOdooPet from '../../assets/dashboard-odoo-pet.png'
@@ -35,13 +51,42 @@ const Projects = () => {
             <h1>Proyectos</h1>
             <div className='projects-page'>
                 <div className='project'>
+                    <h2>CommandApp</h2>
+                    <p>Una aplicación para la gestión de comandas y menús en un restaurante.</p>
+                    <a href='' />
+                    <div className='stack-alt'>
+                        <img src={next} />
+                        <img src={node} />
+                        <img src={mysql} />
+                    </div>
+                    <div className='project-images-pm'>
+                        <img className='pm' src={commandAppDashBoard} />
+                        <img className='pm' src={dailyMenu} />
+                        <img className='pm' src={getCommand} />
+                        <img className='pm' src={Commands} />
+                        <img className='pm' src={CommandDetail} />
+                        <img className='pm' src={TableStatus} />
+                        <img className='pm' src={menus} />
+                        <img className='pm' src={dishes} />
+
+                    </div>
+                    <div className="mobile">
+                        <img className='pm' src={homeMobile} />
+                        <img className='pm' src={burgerMenu} />
+                        <img className='pm' src={dishesMobile} />
+                        <img className='pm' src={tablesMobile} />
+                        <img className='pm' src={menusMobile} />
+                        <img className='pm' src={commandsMobile} />
+                    </div>
+                </div>
+                <div className='project'>
                     <h2>Projects Management</h2>
                     <p>Una aplicación para gestionar proyectos de desarrollo Scrum.</p>
                     <a href='' />
                     <div className='stack-alt'>
-                        <img src={react}/>
-                        <img src={spring}/>
-                        <img src={mysql}/>
+                        <img src={react} />
+                        <img src={spring} />
+                        <img src={mysql} />
                     </div>
                     <div className='project-images-pm'>
                         <img className='pm' src={pm1} />
@@ -64,8 +109,8 @@ const Projects = () => {
                     <p>Una aplicación para buscar información sobre Pokémon conectada con la PokéApi.</p>
                     <a href='' />
                     <div className='stack'>
-                        <img src={react}/>
-                        <img src={extApi}/>
+                        <img src={react} />
+                        <img src={extApi} />
                     </div>
                     <div className='project-images'>
                         <img className='pokimon' src={pokimon} />
@@ -78,9 +123,9 @@ const Projects = () => {
                     <p>Una aplicación para compartir recetas online.</p>
                     <a href='' />
                     <div className='stack-alt'>
-                        <img src={react}/>
-                        <img src={spring}/>
-                        <img src={mysql}/>
+                        <img src={react} />
+                        <img src={spring} />
+                        <img src={mysql} />
                     </div>
                     <div className='project-images-cookbook'>
                         <img className='cooks' src={cookbookCooks} />
@@ -92,7 +137,7 @@ const Projects = () => {
                 <div className='project'>
                     <h2>Wallpaper Pattern Editor</h2>
                     <p>Una aplicación para generar fondos de pantalla basados en patrones de figuras.</p>
-                    <img className='python' src={python}/>
+                    <img className='python' src={python} />
                     <a href='' />
                     <div className='project-images-wallpaper'>
                         <img className='wallpaper-editor' src={wallpaper} />
@@ -102,13 +147,13 @@ const Projects = () => {
                     <h2>Odoo Pet</h2>
                     <p>Una aplicación en Odoo para gestionar la adopción de animales</p>
                     <div className='stack-odoo'>
-                        <img className='python' src={python}/>
-                        <img className='python' src={odoo}/>
+                        <img className='python' src={python} />
+                        <img className='python' src={odoo} />
                     </div>
                     <div className='project-images-odoo-pet'>
-                        <img className='img-odoo-pet' src={odooPet}/>
-                        <img className='img-odoo-pet'src={dashboardOdooPet}/>
-                        <img className='img-odoo-pet'src={formOdoo}/>
+                        <img className='img-odoo-pet' src={odooPet} />
+                        <img className='img-odoo-pet' src={dashboardOdooPet} />
+                        <img className='img-odoo-pet' src={formOdoo} />
                     </div>
                 </div>
             </div>
